ba972cc2c0adad3a854490c54a97fd0461f439e8
Liliana-Paredes <pliliana.mend@gmail.com>
2024-08-20 18:17:08 -0500

    commit del trabajo

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// const btnAbrirModal = document.querySelector("#btn-abir-modal");
+// const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
+
+// btnAbrirModal.addEventListener("click",()=>{
+//     modal.showModal();
+// });
+// btnCerrarModal.addEventListener("click",()=>{
+//     modal.close();
+// });
+
